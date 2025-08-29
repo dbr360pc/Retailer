8e2a29034d2fb989d645912db0dea6aa5b068a02
sangeetha <sangeetha@wizdomwaves.com>
2025-08-29 13:25:29 +0530

    up

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/venv/bin/activate"
 
-python_path="$SCRIPT_DIR/venv/bin/python"
-
-python "$python_path" "$SCRIPT_DIR/deploy.py" "$@"
+"$SCRIPT_DIR/venv/bin/python" "$SCRIPT_DIR/deploy.py" "$@"
